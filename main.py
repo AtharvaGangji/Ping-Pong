2885cc3d1d7ef52ae7f4c727a0c3ffbc564308fa
Atharva Gangji <71313049+AtharvaGangji@users.noreply.github.com>
2021-05-15 12:25:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+# ---------------------------------------------- imports
+import pygame
 
+# ---------------------------------------------- variables
+running = True
+
+
+# ---------------------------------------------- main game function
+def play():
+    pygame.init()  # initialize pygame
+
+    global running  # global variables
+
+    # ---------------------- Game Window
+    window = pygame.display.set_mode((800, 450))  # initialise screen
+
+    while running:  # main while loop
+        for event in pygame.event.get():
+            # ---------- Quit Window
+            if event.type == pygame.QUIT:  # Quit window if close button pressed
+                running = False
+            # ---------- Track Buttons
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:  # Escape Button Quit Window
+                    running = False
+
+        pygame.display.update()  # update screen
+
+
+# ---------------------------------------------- required if statement
+if __name__ == "__main__":
+    play()
